442914d62b7d83840dbc67de4353dd5321af029a
cchartr <tuzepoito@gmail.com>
2018-05-12 16:36:01 +0200

    pixel rendering

--- SanityRush/Assets/CameraController.cs
+++ SanityRush/Assets/CameraController.cs
@@ -6,10 +6,18 @@ public class CameraController : MonoBehaviour {
 
     public GameObject player;
     private Vector3 offset;
+
+    public int pixPerUnit;
+    public float zoom;
+
     // Use this for initialization
     void Start()
     {
         offset = transform.position - player.transform.position;
+
+        // set orthographic size for pixel rendering
+        Camera camera = GetComponent<Camera>();
+        camera.orthographicSize = Screen.height / (2.0f * zoom * pixPerUnit);
     }
 
     // Update is called once per frame
