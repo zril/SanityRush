f69a863aa99d42744fa8aa80f1dd96566f3e3b88
cchartr <tuzepoito@gmail.com>
2018-05-13 00:24:52 +0200

    drugs UI

--- SanityRush/Assets/Scripts/UI.cs
+++ SanityRush/Assets/Scripts/UI.cs
@@ -7,10 +7,13 @@ public class UI : MonoBehaviour {
 
     public Text drugLevel;
     public Text drugTimer;
-    public Text firstDrug;
-    public Text secondDrug;
+    public Image firstDrug;
+    public Image secondDrug;
 
     public Slider drugBar;
+    public Slider drugTimerBar;
+
+    public Sprite[] pillSprites;
 
     public Player player;
 
@@ -20,17 +23,16 @@ public class UI : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         cursorTimer = 0;
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update () {
         // druglevel
-        drugLevel.GetComponent<Text>().text = Mathf.RoundToInt(player.DrugLevel).ToString();
+        drugLevel.text = Mathf.RoundToInt(player.DrugLevel).ToString();
 
         if (cursorTimer == 0)
         {
-            float cursorMaxOffset = 0.02f * Mathf.Abs(player.DrugLevel - 50) / 50f;
+            float cursorMaxOffset = 0.01f * Mathf.Abs(player.DrugLevel - 50) / 50f;
             cursorOffset = Random.Range(-cursorMaxOffset, cursorMaxOffset);
             cursorTimer = Mathf.RoundToInt(Random.Range(0, 5));
             drugBar.value = cursorOffset + player.DrugLevel / 100f;
@@ -41,10 +43,30 @@ public class UI : MonoBehaviour {
         }
 
         //drug timer
-        drugTimer.GetComponent<Text>().text = player.DrugTimer.ToString();
+        drugTimer.text = player.DrugTimer.ToString();
+        drugTimerBar.value = player.DrugTimer;
 
         // drug select
-        firstDrug.GetComponent<Text>().text = player.Drug1.ToString();
-        secondDrug.GetComponent<Text>().text = player.Drug2.ToString();
+        // firstDrug.text = player.Drug1.ToString();
+        // secondDrug.text = player.Drug2.ToString();
+        if (pillSprites[(int)player.Drug1])
+        {
+            firstDrug.enabled = true;
+            firstDrug.sprite = pillSprites[(int)player.Drug1];
+        }
+        else
+        {
+            firstDrug.enabled = false;
+        }
+
+        if (pillSprites[(int)player.Drug2])
+        {
+            secondDrug.enabled = true;
+            secondDrug.sprite = pillSprites[(int)player.Drug2];
+        }
+        else
+        {
+            secondDrug.enabled = false;
+        }
     }
 }
